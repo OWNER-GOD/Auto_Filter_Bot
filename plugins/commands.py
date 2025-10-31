85489c09f9b5489abf518141807baee4c5f12c09
OWNER-GOD <ownergod78@gmail.com>
2025-10-31 20:35:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -99,13 +99,17 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('🎁 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🎁', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+               ],[
+                    InlineKeyboardButton(' 🎬 ɢʀᴏᴜᴘ 🎬', url=f'http://t.me/'),
+                    InlineKeyboardButton('🎥 ᴄʜᴀɴɴᴇʟ 🎥', url=f'http://t.me/)
                 ],[
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
                 ],[
-                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
-                    InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+                    InlineKeyboardButton(' 🛠️ʜᴇʟᴘ 🛠️', callback_data='help'),
+                    InlineKeyboardButton(' 📝ᴀʙᴏᴜᴛ 📝', callback_data='about')
+                ],[
+                    InlineKeyboardButton('🍿ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ 🍿', callback_data="topsearch"),
+                    InlineKeyboardButton('✨ᴜᴘɢʀᴀᴅᴇ ✨', callback_data="premium_info"),
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
