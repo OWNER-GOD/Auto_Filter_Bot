662eb16887980edf23b1488924e23aef274ec475
OWNER-GOD <ownergod78@gmail.com>
2025-11-01 08:41:42 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1448,13 +1448,17 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "start":
         buttons = [[
-                    InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('ϟ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ϟ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+               ],[
+                    InlineKeyboardButton(' ϟ Gʀᴏᴜᴘ ϟ ', url=f'http://t.me/searchmoviestoken'),
+                    InlineKeyboardButton('ϟ Cʜᴀɴɴᴇʟ ϟ', url=f'http://t.me/ownergodupdate'),
                 ],[
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
                 ],[
-                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
-                     InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+                    InlineKeyboardButton(' ϟ Hᴇʟᴘ ϟ ', callback_data='help'),
+                    InlineKeyboardButton(' ϟ Aʙᴏᴜᴛ ϟ ', callback_data='about')
+                ],[
+                    InlineKeyboardButton('ϟ Tᴏᴘ Sᴇᴀʀᴄʜɪɴɢ ϟ', callback_data="topsearch"),
+                    InlineKeyboardButton('ϟ Uᴘɢʀᴀᴅᴇ ϟ', callback_data="premium_info"),
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
